6d88480fad6b5669f006ec0a85203a6605f98d8f
Sabbir Rahman Sakib <sabbir.sakib@cepheid.com>
2020-09-07 20:52:45 +0200

    added more files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="home">
+        <div class="red">
+        red
+        </div>
+        <div class="green">
+        green
+        </div>
+    </div>
+@endsection
